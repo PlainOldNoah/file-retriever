d4a4b137b1b84e601c6a8d9e8d87a8a9ca0d7eb6
Noah MacMinn <plainoldnoah@me.com>
2021-07-31 21:39:29 -0400

    Added file export, deleted progress bar

--- EntryRetriever/Main.gd
+++ EntryRetriever/Main.gd
@@ -1,6 +1,6 @@
 extends PanelContainer
 
-const windowSize = Vector2(800, 360)
+const windowSize = Vector2(800, 320)
 const bigWindowSize = Vector2(800, 720)
 
 var inputFolderFormat = "Logs - *"
@@ -10,7 +10,7 @@ var entryHeaderFormat = "*/*/*, *"
 enum INPUT_TYPES {DIRECTORY, FILE, FILES}
 var inputType
 
-var printEntry:bool = false
+var printEntry:bool = true
 var printDate:bool = true
 
 var searchKeyArray : PoolStringArray = []
@@ -43,6 +43,7 @@ func _ready():
 	reset_Statistics()
 	rng.randomize()
 
+
 func reset_Statistics():
 	FileCountOutput.text = "0"
 	EntryCountOutput.text = "0"
@@ -62,22 +63,18 @@ func directory_Iterate(path):
 		while file_name != "":
 			#Filters out Non-Log Folders
 			if dir.current_is_dir() and file_name.match(inputFolderFormat):
-#				print("Directory: " + file_name)
 				directory_Iterate(dir.get_current_dir() + "/" + file_name)
 			#Filters out files that don't have the right naming scheme
 			elif file_name.match(inputFileFormat):
-#				print("File: " + file_name)
 				read_File(dir.get_current_dir() + "/" + file_name)
 			file_name = dir.get_next()
+			
 	else:
 		print_debug("ERROR: Bad Path Parameter: ", path)
 
 
 func read_File(file):
 	FileCountOutput.text = str(int(FileCountOutput.text) + 1)
-	
-	#TODO
-#	LoadingBar.value = ((int(FileCountOutput.text) / 55) * 100)
 	
 	var f = File.new()
 	f.open(file, File.READ)
@@ -100,6 +97,7 @@ func read_File(file):
 					line = f.get_line()
 					
 					while not line.strip_escapes().empty():
+						
 						print_to_output(line)
 						line = f.get_line()
 					OutputTextBox.bbcode_text += "\n"
@@ -164,27 +162,32 @@ func generate_rand_date() -> String:
 func _on_Run_pressed():
 	reset_Statistics()
 	parse_Search_Keys()
-	#TODO: This should be temporary and reworked later
-	#This checks the input field to see if a directory or txt file is input
+	
+	StatusBarOutput.text = "Running"
+	yield(get_tree().create_timer(0.02), "timeout")
+	
+#	#TODO: This should be temporary and reworked later
+#	#This checks the input field to see if a directory or txt file is input
 	if SelectionInput.text.match("*.txt"):
 		inputType = INPUT_TYPES.FILE
 	else:
 		inputType = INPUT_TYPES.DIRECTORY
 	
-	StatusBarOutput.text = "Running"
-	var starTime = OS.get_ticks_msec()
+	var startTime = OS.get_ticks_msec()
 	
 	match inputType:
 		INPUT_TYPES.DIRECTORY:
 			directory_Iterate(SelectionInput.text)
 		INPUT_TYPES.FILE:
 			read_File(SelectionInput.text)
-	ElapsedTimeOutput.text = str((OS.get_ticks_msec() - starTime) / 1000.0)
+	
+	ElapsedTimeOutput.text = str((OS.get_ticks_msec() - startTime) / 1000.0)
 	StatusBarOutput.text = "Finished"
 
 
 func _on_Clear_pressed():
 	reset_Statistics()
+
 
 func _on_Quit_pressed():
 	get_tree().quit()
@@ -193,27 +196,47 @@ func _on_Quit_pressed():
 func _on_Search_Keys_text_changed(_new_text):
 	parse_Search_Keys()
 
+
 func parse_Search_Keys():
 	searchKeyArray = SearchKeyInput.text.split(",", true, 0)
 	for i in searchKeyArray.size():
 		searchKeyArray[i] = searchKeyArray[i].strip_edges()
 
+
 func _on_ExportBtn_pressed():
-	pass # Replace with function body.
+	var outputFileName:String = "newFile"
+	
+	if searchKeyArray.size() > 0:
+		outputFileName = str(searchKeyArray[0])
+	outputFileName = outputFileName\
+	.replace("/", "_").replace("\\", "_").replace(":", "_")\
+	.replace("*", "_").replace("?", "_").replace("\"", "_")\
+	.replace("<", "_").replace(">", "_").replace("|", "_")\
+	
+	print(outputFileName)
+	
+	var outputFilePath = "user://" + outputFileName + ".txt"
+	
+	var file2Check = File.new()
+	var doFileExist = file2Check.file_exists(outputFilePath)
+	
+	
+	var file = File.new()
+	var fileStatus = 3
+	if AppendButton.pressed == false or not doFileExist:
+		fileStatus = 2
+
+	file.open(outputFilePath, fileStatus)
+	StatusBarOutput.text = "File Saved: " + OS.get_user_data_dir()
+	file.store_string(OutputTextBox.bbcode_text)
+	file2Check.close()
+	file.close()
 
 
 func _on_FileSelect_pressed():
 	InputFileDialog.popup_centered_ratio(1.0)
 	InputFileDialog.current_path = SelectionInput.text
 	InputFileDialog.current_dir = SelectionInput.text
-
-func _on_Append_pressed():
-	if AppendButton.pressed == true:
-		OverwriteButton.pressed = false
-
-func _on_Overwrite_pressed():
-	if OverwriteButton.pressed == true:
-		AppendButton.pressed = false
 
 
 func _on_WindowToggleBtn_toggled(button_pressed):
@@ -224,21 +247,26 @@ func _on_WindowToggleBtn_toggled(button_pressed):
 	if not button_pressed:
 		OS.min_window_size = windowSize
 		OS.window_size.y = windowSize.y
+	OS.set_window_position(OS.get_screen_size()*0.5 - OS.get_window_size()*0.5)
+
 
 #File Input
 func _on_SingleInputFileDialog_dir_selected(dir):
 	inputType = INPUT_TYPES.DIRECTORY
 	SelectionInput.text = dir
 
+
 func _on_SingleInputFileDialog_file_selected(path):
 	inputType = INPUT_TYPES.FILE
 	SelectionInput.text = path
+
 
 #Can only print entries if the date is printed as well.
 func _on_PrintDate_toggled(button_pressed):
 	printDate = button_pressed
 	if printDate == false:
 		printEntryButton.pressed = false
+
 
 func _on_PrintEntry_toggled(button_pressed):
 	printEntry = button_pressed
@@ -247,4 +275,7 @@ func _on_PrintEntry_toggled(button_pressed):
 
 
 func _on_RandomDate_pressed():
-	SearchKeyInput.text = generate_rand_date()
+	if not searchKeyArray.empty():
+		SearchKeyInput.text += ", "
+	SearchKeyInput.text += generate_rand_date()
+	parse_Search_Keys()
